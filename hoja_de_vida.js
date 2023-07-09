f155c22d0772dd05da416fdc378eada66b425db8
tronprogram <60573173+tronprogram@users.noreply.github.com>
2023-07-09 09:24:35 -0500

    Add files via upload

--- hoja_de_vida.js
+++ hoja_de_vida.js
@@ -1,5 +1,5 @@
 document.getElementById("btnPasajes").addEventListener("click", function() {
   
  
-    window.location.href = "https://enlace-a-la-cancion-sobreviviendo.com";
+    window.location.href = "https://drive.google.com/drive/folders/1VN_huED-drQzYtu8Ge4QtK5yYtPUKVvc?usp=sharing";
     });
